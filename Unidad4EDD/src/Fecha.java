69752ad43788f0df9ad7c43f0df9fb3e43804ea1
Villu <Villu@DESKTOP-6O4GSD6>
2017-03-16 19:15:11 +0100

    Separamos método bisiesto()

--- Unidad4EDD/src/Fecha.java
+++ Unidad4EDD/src/Fecha.java
@@ -4,11 +4,15 @@ public class Fecha {
 	private int dia; 
 	private int mes;
 	private int anio;
-
+	
 	public Fecha (int dia, int mes, int anio) {
 		this.dia = dia; this.mes = mes; this.anio = anio;
 	}
-		
+	private boolean bisiesto ( ) {
+		if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100 != 0)) )
+			return true;
+		else return false;
+		}
 	private int diasMes( ) {
 		int diasMes = 0;
 		switch (mes) {
@@ -25,20 +29,19 @@ public class Fecha {
 			case 9:
 			case 11 : diasMes = 30;
 			break;
-			case 2 : 
-				if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100 != 0)) )
+			case 2 : if ( bisiesto() )
 							diasMes = 29;
 						else
 							diasMes = 28;
 						break;
 		}
-			return diasMes;	
+		return diasMes;	
 	}
-		public boolean valida ( ) {
-			if (dia < 1 || dia > 31) return false;
-			if (mes < 1 || mes > 12) return false;
-			if (anio < 0) return false;
-			if (dia > diasMes()) return false;
-			else return true;
-		}
+	public boolean valida ( ) {
+		if (dia < 1 || dia > 31) return false;
+		if (mes < 1 || mes > 12) return false;
+		if (anio < 0) return false;
+		if (dia > diasMes()) return false;
+		else return true;
+	}
 }
