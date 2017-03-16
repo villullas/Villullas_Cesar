ac27514e31f71acbc669df326ccc760a5959fee0
Villu <Villu@DESKTOP-6O4GSD6>
2017-03-16 19:07:44 +0100

    Separamos clase TestFecha

--- Unidad4EDD/src/Fecha.java
+++ Unidad4EDD/src/Fecha.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 
 public class Fecha {
 	
@@ -43,22 +39,5 @@ public class Fecha {
 		if (dia > diasMes) return false;
 		else return true;
 		}
-
-	public static void main(String[] args)throws IOException {
-		int dia, mes, anyo;
-		System.out.println ("Introduce un día: ");
-		BufferedReader entrada = new BufferedReader (new InputStreamReader (System.in));
-		dia = Integer.parseInt(entrada.readLine());
-		System.out.println ("Introduce un mes: ");
-		mes = Integer.parseInt(entrada.readLine());
-		System.out.println ("Introduce un año: ");
-		anyo = Integer.parseInt(entrada.readLine());
-		Fecha f1=new Fecha(dia,mes,anyo);
-		if (f1.valida())
-			System.out.println("La fecha: "+dia+"/"+mes+"/"+anyo+" es válida");
-		else
-			System.out.println("La fecha: "+dia+"/"+mes+"/"+anyo+" NO es válida");
-	}
-
 
 }
