44299c9c8b248d6802e0d16bcc2d54b4314e42e4
Villu <Villu@DESKTOP-6O4GSD6>
2017-03-16 19:12:16 +0100

    Separamos método valida()

--- Unidad4EDD/src/Fecha.java
+++ Unidad4EDD/src/Fecha.java
@@ -4,15 +4,12 @@ public class Fecha {
 	private int dia; 
 	private int mes;
 	private int anio;
-	
+
 	public Fecha (int dia, int mes, int anio) {
 		this.dia = dia; this.mes = mes; this.anio = anio;
 	}
-
-	public boolean valida ( ) {
-		if (dia < 1 || dia > 31) return false;
-		if (mes < 1 || mes > 12) return false;
-		if (anio < 0) return false;
+		
+	private int diasMes( ) {
 		int diasMes = 0;
 		switch (mes) {
 			case 1:
@@ -28,16 +25,20 @@ public class Fecha {
 			case 9:
 			case 11 : diasMes = 30;
 			break;
-			case 2 :
+			case 2 : 
 				if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100 != 0)) )
-					diasMes = 29;
-				else
-					diasMes = 28;
-				break;
+							diasMes = 29;
+						else
+							diasMes = 28;
+						break;
 		}
-	
-		if (dia > diasMes) return false;
-		else return true;
+			return diasMes;	
+	}
+		public boolean valida ( ) {
+			if (dia < 1 || dia > 31) return false;
+			if (mes < 1 || mes > 12) return false;
+			if (anio < 0) return false;
+			if (dia > diasMes()) return false;
+			else return true;
 		}
-
 }
